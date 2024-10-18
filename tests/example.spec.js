f9a8d05443b563f49dbed965a2337fcfc1e31048
Your Name <you@example.com>
2024-10-19 01:58:30 +0300

    fix

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,12 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
-  await page.goto('https://commitquality.com/add-product');
-  await page.getByTestId('product-textbox').click();
-  await page.getByTestId('product-textbox').fill('Sniper');
-  await page.getByTestId('price-textbox').click();
-  await page.getByTestId('price-textbox').fill('100');
-  await page.getByTestId('date-stocked').fill('2024-10-04');
-  await page.getByTestId('submit-form').click();
-  await expect(page).toHaveTitle('https://commitquality.com/');
+test("test", async ({ page }) => {
+  await page.goto("https://commitquality.com/add-product");
+  await page.getByTestId("product-textbox").click();
+  await page.getByTestId("product-textbox").fill("Sniper");
+  await page.getByTestId("price-textbox").click();
+  await page.getByTestId("price-textbox").fill("100");
+  await page.getByTestId("date-stocked").fill("2024-10-04");
+  await page.getByTestId("submit-form").click();
+  await expect(page).toHaveTitle("CommitQuality - Test Automation Demo");
 });
